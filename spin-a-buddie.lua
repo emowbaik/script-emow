7d8f77cb7a445378f94693e46bf61cfe70a6d618
Emowmow <76246974+emowbaik@users.noreply.github.com>
2026-01-15 00:22:44 +0000

    Enchane ConfigurationSaving

--- spin-a-buddie.lua
+++ spin-a-buddie.lua
@@ -10,9 +10,6 @@ local Window = Rayfield:CreateWindow({
 
    ToggleUIKeybind = "K", -- The keybind to toggle the UI visibility (string like "K" or Enum.KeyCode)
 
-   DisableRayfieldPrompts = false,
-   DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
-
    ConfigurationSaving = {
       Enabled = true,
       FolderName = nil, -- Create a custom folder for your hub/game
@@ -1090,4 +1087,4 @@ local ExplorePathButton = OtherTab:CreateButton({
    end,
 })
 
-
+Rayfield:LoadConfiguration() -- Load the configuration file
